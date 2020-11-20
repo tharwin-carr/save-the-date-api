4a0e83b2726cf1a5f77207af5d0b84c4f7b2eff3
Tharwin <tharwin.carr08@gmail.com>
2020-11-20 11:40:37 -0800

    added the db

---
+++ src/dates/dates-router.js
@@ -0,0 +1,104 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const DatesService = require('./dates-service')
+
+const datesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeDate = date => ({
+    id: date.id,
+    dateDescription: xss(date.dateDescription)
+})
+
+datesRouter
+.route('/dates')
+.get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    DatesService.getAllDates(knexInstnace)
+    .then(dates => {
+        res.json(dates.map(serializeDate))
+    })
+    .catch(next)
+})
+.post(jsonParser, (req, res, next) => {
+    const { dateDescription } = req.body
+    const newDate = {dateDescription}
+
+    for (const [key, value] of Object.entries(newDate)) {
+        if (value == null) {
+            return res.status(400).json({
+                error: { message:  `Missing '${key}' in request body` }
+            })
+        }
+    }
+
+    newDate.dateDescription = dateDescription
+    DatesService.insertDate(
+        req.app.get('db'),
+        newDate
+    )
+    .then(date => {
+        res
+            .status(201)
+            .location(path.posix.join(req.originalUrl, `/${date.id}`))
+            .json(serializeDate(date))
+    })
+    .catch(next)
+})
+
+datesRouter
+.route('/:id')
+.all((req, res, next) => {
+    DatesService.getById(
+        req.app.get('db'),
+        req.params.id
+    )
+    .then(date => {
+        if(!date) {
+            return res.status(404).json({
+                error: { message: `Date doesn't exist` }
+            })
+        }
+        res.date = date
+        next()
+    })
+    .catch(next)
+})
+.get((req, res, next) => {
+    res.json(serializeDate(res.date))
+})
+.patch(jsonParser, (req, res, next) => {
+    const { dateDescription } = req.body
+    const dateToUpdate = dateDescription
+
+    const numberOfValues = Object.values(dateToUpdate).filter(Boolean).length
+    if(numberOfValues === 0) {
+        return res.status(400).json({
+            error: {
+                message: `Request body must contain dateDescription`
+            }
+        })
+    }
+    DatesService.updateDate(
+        req.app.get('db'),
+        req.params.id,
+        dateToUpdate
+    )
+    .then(numRowsAffected => {
+        res.status(204).end()
+    })
+    .catch(next)
+})
+.delete((req, res, next) => {
+    DatesService.deleteDate(
+        req.app.get('db'),
+        req.params.id
+    )
+    .then((numRowsAffected) => {
+        res.status(204).end()
+    })
+    .catch(next)
+})
+
+module.exports = datesRouter
---
+++ src/dates/dates-service.js
@@ -0,0 +1,33 @@
+const DatesService = {
+    getAllDates(knex) {
+        return knex.select('*').from('savethedate_dates')
+    },
+    insertDate(knex, newDate) {
+        return knex
+            .insert(newDate)
+            .into('savethedate_dates')
+            .returng('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    getById(knex, id) {
+        return knex
+            .from('savethedate_dates')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+    deleteDate(knex, id) {
+        return knex('savethedate_dates')
+        .where({ id })
+        .delete()
+    },
+    updateDate(knex, id, newDateFields) {
+        return knex('savethedate_dates')
+        .where({ id })
+        .update(newDateFields)
+    }
+}
+
+module.exports = DatesService
