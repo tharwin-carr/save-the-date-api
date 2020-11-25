b3c1001674ea4e2a876e271596916c69e67c3a53
Tharwin <tharwin.carr08@gmail.com>
2020-11-24 18:07:31 -0800

    getting ready for produciton

--- src/dates/dates-service.js
+++ src/dates/dates-service.js
@@ -1,11 +1,11 @@
 const DatesService = {
     getAllDates(knex) {
-        return knex.select('*').from('dates')
+        return knex.select('*').from('savethedate_dates')
     },
     insertDate(knex, newDate) {
         return knex
             .insert(newDate)
-            .into('dates')
+            .into('savethedate_dates')
             .returng('*')
             .then(rows => {
                 return rows[0]
@@ -13,18 +13,18 @@ const DatesService = {
     },
     getById(knex, id) {
         return knex
-            .from('dates')
+            .from('savethedate_dates')
             .select('*')
             .where('id', id)
             .first()
     },
     deleteDate(knex, id) {
-        return knex('dates')
+        return knex('savethedate_dates')
         .where({ id })
         .delete()
     },
     updateDate(knex, id, newDateFields) {
-        return knex('dates')
+        return knex('savethedate_dates')
         .where({ id })
         .update(newDateFields)
     }
