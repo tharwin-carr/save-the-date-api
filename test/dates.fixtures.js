f157516520524d27d2f2f1cd10b8c38fb97462dd
Tharwin <tharwin.carr08@gmail.com>
2020-11-26 12:16:20 -0800

    fixed typo

--- test/dates.fixtures.js
+++ test/dates.fixtures.js
@@ -2,7 +2,7 @@ function makeDatesArray() {
     return [
         {
             id: 1,
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.'
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobs qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.'
         },
         {
             id: 2,
